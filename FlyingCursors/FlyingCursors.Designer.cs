66b73435b9240e44dd2c7d8aabd64d0c5ec97927
Netdex <netdex593@gmail.com>
2016-06-25 17:55:25 -0400

    Initial Commit

---
+++ FlyingCursors/FlyingCursors.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace FlyingCursors
+{
+    partial class FlyingCursors
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timerTick = new System.Windows.Forms.Timer(this.components);
+            this.timerSpawn = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timerTick
+            // 
+            this.timerTick.Enabled = true;
+            this.timerTick.Interval = 16;
+            this.timerTick.Tick += new System.EventHandler(this.timerTick_Tick);
+            // 
+            // timerSpawn
+            // 
+            this.timerSpawn.Enabled = true;
+            this.timerSpawn.Interval = 1000;
+            this.timerSpawn.Tick += new System.EventHandler(this.timerSpawn_Tick);
+            // 
+            // FlyingCursors
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(375, 369);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FlyingCursors";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "MOTHERFUCKING FLYING CURSORS";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.Lime;
+            this.Load += new System.EventHandler(this.FlyingCursors_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FlyingCursors_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timerTick;
+        private System.Windows.Forms.Timer timerSpawn;
+    }
+}
+
---
+++ FlyingCursors/FlyingCursors.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlyingCursors
+{
+    public partial class FlyingCursors : Form
+    {
+        public FlyingCursors()
+        {
+            InitializeComponent();
+        }
+
+        private Cursor _cursor;
+        private List<CursorSprite> _sprites;
+        private Random _random;
+
+        private void FlyingCursors_Load(object sender, EventArgs e)
+        {
+            _cursor = Cursor.Current;
+            _sprites = new List<CursorSprite>();
+            _random = new Random();
+            Width = Screen.PrimaryScreen.Bounds.Width;
+            Height = Screen.PrimaryScreen.Bounds.Height;
+        }
+
+        
+        private void FlyingCursors_Paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+            foreach (var sp in _sprites)
+            {
+                _cursor.Draw(g, new Rectangle(sp.LocationX, sp.LocationY, 50, 50));
+            }
+        }
+
+        private void timerTick_Tick(object sender, EventArgs e)
+        {
+            foreach (CursorSprite sp in _sprites)
+            {
+                sp.LocationX += sp.VelocityX;
+                sp.LocationY += sp.VelocityY;
+            }
+            Refresh();
+        }
+
+        private void timerSpawn_Tick(object sender, EventArgs e)
+        {
+            CursorSprite sp = new CursorSprite(_random.Next(Width), _random.Next(Height), _random.Next(20) - 10, _random.Next(20) - 10);
+            _sprites.Add(sp);
+        }
+    }
+
+    class CursorSprite
+    {
+        public int LocationX { get; set; }
+        public int LocationY { get; set; }
+        public int VelocityX { get; set; }
+        public int VelocityY { get; set; }
+
+        public CursorSprite(int locx, int locy, int velx, int vely)
+        {
+            LocationX = locx;
+            LocationY = locy;
+            VelocityX = velx;
+            VelocityY = vely;
+        }
+    }
+}
